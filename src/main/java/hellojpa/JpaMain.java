2cb2cd7d28e98f4eb26245b46338800a45f1f2e9
홍성민 <honggasm@gmail.com>
2021-01-07 00:34:10 +0900

    jpa dirty checking test

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,29 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = em.find(Member.class, 2L);
+            
+            //값마 변경해도 update 쿼리가 실행됨
+            member.setName("이름 변경");
+            System.out.println("==========");
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
