2c2d481d26d551f01c826df7387e2818b7412fa7
홍성민 <honggasm@gmail.com>
2021-01-07 00:44:20 +0900

    1차 cache 조회 예시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -13,17 +13,8 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 비영속
-            Member newMember = new Member();
-            newMember.setId(101L);
-            newMember.setName("name");
-            
-            // 영속
-            System.out.println("BEFORE==========");
-            em.persist(newMember);
-            System.out.println("AFTER==========");
-            
-            Member findMember = em.find(Member.class, 101L);
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
