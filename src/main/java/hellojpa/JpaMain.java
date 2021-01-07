d4f44740bfaf5765215cd17e5c1e07a124691e02
홍성민 <honggasm@gmail.com>
2021-01-08 00:30:07 +0900

    엔티티 맵핑 실습

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -5,6 +5,9 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+
+
+
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
@@ -13,14 +16,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 영속
-            Member member = em.find(Member.class, 101L);
-            member.setName("변경실패");
-            
-            // 엔티티 메니저안에 있는 모든 엔티티를 준영속 상태로 변경
-            em.clear();
-            member = em.find(Member.class, 101L);
-            
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
