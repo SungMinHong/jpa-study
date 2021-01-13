463e7152296d1770961cef5137ccb6d6080ec843
홍성민 <honggasm@gmail.com>
2021-01-13 17:52:13 +0900

    상속관계 맵핑: 조인

--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -3,7 +3,7 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Item {
     @Id @GeneratedValue
     private Long id;
