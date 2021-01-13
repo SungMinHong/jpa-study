7516eb9831d480002ce2e147eb3d972d8f783193
홍성민 <honggasm@gmail.com>
2021-01-13 17:51:58 +0900

    상속관계 맵핑: 싱글 테이블

---
+++ src/main/java/hellojpa/Item.java
@@ -0,0 +1,13 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public class Item {
+    @Id @GeneratedValue
+    private Long id;
+    
+    private String name;
+    private int price;
+}
--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,37 +15,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-            //팀 저장
-            System.out.println("팀 저장");
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team);
-            
-            //회원 저장 
-            System.out.println("회원 저장");
-            Member member = new Member();
-            member.setUsername("member1");
-            member.changeTeam(team);
-            em.persist(member);
-
-            // 양방향 맵핑이기 때문에 team내 member 리스트에도 member를 넣어주는 습관을 갖자
-            // 이는 순수 객체 상태를 고려했기 때문
-            team.getMembers().add(member);
-
-            em.flush(); // DB에 쿼리 반영
-            em.clear(); // 1차 캐시(영속성 컨텍스트) 초기화
-            
-            System.out.println("멤버 찾기");
-            Member findMember = em.find(Member.class, member.getId());
-            List<Member> members = findMember.getTeam().getMembers();
-
-            System.out.println("team내 멤버 이름 출력");
-            members.stream()
-                    .map(Member::getUsername)
-                    .forEach((name) -> System.out.println("memberName: " + name));
-            
-            System.out.println("트랜잭션 커밋");
-            tx.commit();
         } catch (Exception e) {
             tx.rollback();
         } finally {
