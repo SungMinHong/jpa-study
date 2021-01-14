89b532ac9520aa21c385e4cf37bd55a45d13c4bf
홍성민 <honggasm@gmail.com>
2021-01-15 00:21:18 +0900

    고아객체: orphanRemoval true

--- src/main/java/hellojpa/Parent.java
+++ src/main/java/hellojpa/Parent.java
@@ -11,7 +11,7 @@ public class Parent {
     
     private String name;
     
-    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Child> childList = new ArrayList<>();
 
     public void addChild(Child child) {
@@ -32,5 +32,9 @@ public class Parent {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Child> getChildList() {
+        return childList;
     }
 }
