a6cc5b93aac2b85e322be177e1c5ec0e6c75d17d
홍성민 <honggasm@gmail.com>
2021-01-19 21:23:07 +0900

    [jpql] fetch 조인

--- src/main/java/JpaMain.java
+++ src/main/java/JpaMain.java
@@ -14,25 +14,44 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("teamA");
+            em.persist(teamA);
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(100);
-            member.changeTeam(team);
-            em.persist(member);
+            Team teamB = new Team();
+            teamB.setName("teamB");
+            em.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setAge(100);
+            member1.changeTeam(teamA);
+            em.persist(member1);
+            
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setAge(33);
+            member2.changeTeam(teamA);
+            em.persist(member2);
+            
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setAge(22);
+            member3.changeTeam(teamB);
+            em.persist(member3);
             
             em.flush();
             em.clear();
             System.out.println("=============================");
             
-            String query = "SELECT m FROM Member AS m INNER JOIN m.team t";
+            String query = "SELECT m FROM Member AS m JOIN FETCH m.team";
             List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
+            
             System.out.println("=============================");
-            System.out.println(result.get(0).getTeam().getName());
+            for (Member m : result) {
+                System.out.println("member: " + m.getUsername() + " " + m.getTeam().getName());
+            }
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
